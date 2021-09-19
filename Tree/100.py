a6c35957ca923a22fa8b1d141f4f115a777ea9de
Jason He <hezhoux@qq.com>
2021-09-19 23:52:43 +0800

    Update 100.py

--- Tree/100.py
+++ Tree/100.py
@@ -6,4 +6,4 @@ class Solution:
     		return False
     	if p.val != q.val:
     		return False
-    	return self.isSameTree(p.left, q.left) && self.isSameTree(p.right, q.right)
+    	return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
