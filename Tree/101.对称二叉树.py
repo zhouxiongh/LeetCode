7b6d286b6e0f9827213e7bafaaa0a44791bf9a5b
Jason He <hezhoux@qq.com>
2021-09-19 23:57:41 +0800

    Update 101.对称二叉树.py

--- Tree/101.对称二叉树.py
+++ Tree/101.对称二叉树.py
@@ -13,13 +13,12 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
-        return self.isMirror(root, root)
-
-    def isMirror(self, node1, node2) -> bool:
-        if not node1 and not node2:
-            return True
-        if not node1 or not node2:
-            return False
-        return node1.val == node2.val and self.isMirror(node1.left, node2.right) and self.isMirror(node1.right, node2.left)
+        def isMirror(node1, node2) -> bool:
+            if not node1 and not node2:
+                return True
+            if not node1 or not node2:
+                return False
+            return node1.val == node2.val and isMirror(node1.left, node2.right) and isMirror(node1.right, node2.left)
+        return isMirror(root, root)
 # @lc code=end
 
