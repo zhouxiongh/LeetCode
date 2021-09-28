d2c2e854964caadb364dc03498714d9118c48f9e
Jason He <hezhoux@qq.com>
2021-09-28 15:46:20 +0800

    Create 297.二叉树的序列化与反序列化.go
    go version

---
+++ Tree/297.二叉树的序列化与反序列化.go
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode.cn id=297 lang=golang
+ *
+ * [297] 二叉树的序列化与反序列化
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type Codec struct {
+    
+}
+
+func Constructor() (_ Codec) {
+	return
+    
+}
+
+// Serializes a tree to a single string.
+func (this *Codec) serialize(root *TreeNode) string {
+	sb := &strings.Builder{}
+	var dfs func(*TreeNode)
+	dfs = func(node *TreeNode) {
+		if node == nil {
+			sb.WriteString("null,")
+			return
+		}
+		sb.WriteString(strconv.Itoa(node.Val))
+		sb.WriteString(",")
+		dfs(node.Left)
+		dfs(node.Right)
+	}
+	dfs(root)
+	return sb.String()
+    
+}
+
+// Deserializes your encoded data to tree.
+func (this *Codec) deserialize(data string) *TreeNode {    
+	sp := strings.Split(data, ",")
+	var build func() *TreeNode
+	build = func() *TreeNode {
+		if sp[0] == "null" {
+			sp = sp[1:]
+			return nil
+		}
+		val, _ := strconv.Atoi(sp[0])
+		sp = sp[1:]
+		return &TreeNode{val, build(), build()}
+	}
+	return build()
+    
+}
+
+
+/**
+ * Your Codec object will be instantiated and called as such:
+ * ser := Constructor();
+ * deser := Constructor();
+ * data := ser.serialize(root);
+ * ans := deser.deserialize(data);
+ */
+// @lc code=end
+
