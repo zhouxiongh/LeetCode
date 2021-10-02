bd79a31168d0217def7fd369a8b5a9f1cdc8fc70
Jason He <hezhoux@qq.com>
2021-10-02 13:36:05 +0800

    Update 169.多数元素.py

--- Tree/169.多数元素.py
+++ Tree/169.多数元素.py
@@ -7,15 +7,28 @@
 # @lc code=start
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        d = {}
-        for n in nums:
-            if n not in d:
-                d[n] = 1
-            else:
-                d[n] += 1
-            if d[n] > len(nums)//2:
-                break
-        return max(d, key=d.get)
+        def vote(nums):
+            tmp = nums[0]
+            count = 0
+            for n in nums:
+                if tmp == n:
+                    count += 1
+                else:
+                    count -= 1
+                    if count == 0: 
+                        tmp = n
+                        count = 1
+            return tmp
+        return vote(nums)
+        # d = {}
+        # for n in nums:
+            # if n not in d:
+                # d[n] = 1
+            # else:
+                # d[n] += 1
+            # if d[n] > len(nums)//2:
+                # break
+        # return max(d, key=d.get)
 
 # @lc code=end
 
