9fa68f7be555d5a072f35a7bd3639f0d860fd6f4
Jason He <hezhoux@qq.com>
2021-10-13 06:46:18 +0800

    Create 445.两数相加-ii.py

---
+++ 445.两数相加-ii.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode.cn id=445 lang=python3
+#
+# [445] 两数相加 II
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        # def reverse(l1):
+            # pass
+        # def addTwoNumbers1(l1, l2):
+            # pass
+        # return reverse(addTwoNumbers1(reverse(l1), reverse(l2)))
+
+        # 进阶：如果输入链表不能修改该如何处理？
+        # 换句话说，不能对列表中的节点进行翻转
+        # use stack
+        s1 = []
+        s2 = []
+        while l1:
+            s1.append(l1.val)
+            l1 = l1.next
+        while l2:
+            s2.append(l2.val)
+            l2 = l2.next
+        head = None
+        sum = 0
+        while len(s1) or len(s2) or sum:
+            sum += s1.pop() if len(s1) else 0
+            sum += s2.pop() if len(s2) else 0
+            n = ListNode(sum % 10)
+            sum //= 10
+            n.next = head
+            head = n
+        return head
+
+
+# @lc code=end
+
