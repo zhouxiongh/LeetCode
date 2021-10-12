2f2e1f5899d6044c407dfd7d00d833c8c25564d2
Jason He <hezhoux@qq.com>
2021-10-12 11:12:32 +0800

    Create 2.两数相加.py

---
+++ 2.两数相加.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode.cn id=2 lang=python3
+#
+# [2] 两数相加
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        # o(n) , int 可能存不下
+        # def l2n(l1):
+            # pass
+        # def n2l(n1):
+            # pass
+        # return n2l(l2n(l1) + l2n(l2))
+        ans = tail =  ListNode(0)
+        s = 0
+        while l1 or l2 or s:
+            s += (l1.val if l1 else 0) + (l2.val if l2 else 0)
+            tail.next = ListNode(s%10)
+            tail = tail.next
+            s //= 10
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return ans.next
+# @lc code=end
+
