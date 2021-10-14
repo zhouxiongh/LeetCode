e9967fffd9083bf86a0dd621813f32c7999fd237
Jason He <hezhoux@qq.com>
2021-10-14 17:07:44 +0800

    Create 23.合并k个升序链表.java

---
+++ 23.合并k个升序链表.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=23 lang=java
+ *
+ * [23] 合并K个升序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        Queue<ListNode> pq = new PriorityQueue<>((v1, v2) -> v1.val - v2.val);
+        for (ListNode node: lists) {
+            if (node != null) {
+                pq.offer(node);
+            }
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (!pq.isEmpty()) {
+            ListNode minNode = pq.poll();
+            tail.next = minNode;
+            tail = minNode;
+            if (minNode.next != null) {
+                pq.offer(minNode.next);
+            }
+        }
+        return dummy.next;
+    }
+}
+// @lc code=end
+
