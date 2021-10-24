0cfaa8e8a2e03bf90f882758106067c149025215
Jason He <hezhoux@qq.com>
2021-10-24 12:48:09 +0800

    Update 46.全排列.py

--- 46.全排列.py
+++ 46.全排列.py
@@ -7,22 +7,22 @@ from typing import List
 # @lc code=start
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def dfs(depth, curr, ans):
+        def dfs(depth, cur=[]):
             if depth == n:
-                ans.append(curr[:])
+                ans.append(cur[:])
                 return
             for i in range(n):
                 if self.used[i]:
                     continue
                 self.used[i] = True
-                curr.append(nums[i])
-                dfs(depth+1, curr, ans)
-                curr.pop()
+                cur.append(nums[i])
+                dfs(depth+1, cur)
+                cur.pop()
                 self.used[i] = False
         ans = []
         self.used = dict.fromkeys(range(len(nums)), False)
         n = len(nums)
-        dfs(0, [], ans)
+        dfs(0)
         return ans
 # @lc code=end
 input_data = [1,2,3]
