d3af858c8ce8e6eb4f2081d399ff78b550377f8e
Jason He <hezhoux@qq.com>
2021-10-26 18:30:30 +0800

    Create 875.爱吃香蕉的珂珂.py

---
+++ 875.爱吃香蕉的珂珂.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=875 lang=python3
+#
+# [875] 爱吃香蕉的珂珂
+#
+
+# @lc code=start
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        low_speed = 1
+        max_speed = max(piles)
+        target_hours = h
+        while low_speed < max_speed:
+            speed = low_speed + (max_speed - low_speed) // 2
+            eat_hours = 0
+            for p in piles:
+                # hours += p // speed
+                eat_hours += (p + speed - 1) // speed
+            if eat_hours <= target_hours:
+                max_speed = speed
+            else:
+                low_speed = speed + 1
+        return low_speed
+            
+# @lc code=end
+
