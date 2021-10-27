dd7f0d5bc8655657fc76468bab7a05128c146803
Jason He <hezhoux@qq.com>
2021-10-27 10:33:54 +0800

    Create 133.克隆图.py
    bfs version

---
+++ 133.克隆图.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode.cn id=133 lang=python3
+#
+# [133] 克隆图
+#
+
+# @lc code=start
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    # def __init__(self):
+    #     self.visited = {}
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return node
+        visited = {}
+        queue = deque([node])
+        visited[node] = Node(node.val, [])
+
+        while queue:
+            n = queue.popleft()
+            for neighbor in n.neighbors:
+                if neighbor not in visited:
+                    visited[neighbor] = Node(neighbor.val)
+                    queue.append(neighbor)
+                visited[n].neighbors.append(visited[neighbor])
+        return visited[node]
+        
+# @lc code=end
+
