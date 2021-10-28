eaa45e5388f980529f28736020205c42b5de2c88
Jason He <hezhoux@qq.com>
2021-10-28 18:45:40 +0800

    Update 207.课程表.py

--- 207.课程表.py
+++ 207.课程表.py
@@ -7,26 +7,41 @@
 # @lc code=start
 class Solution:
     class State:
-        unkonwn = 0
-        visiting = 1
-        visited = 2
+        unvisited = 0
+        visiting = 1 # 临时标记
+        visited = 2 # 永久标记
 
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        def dfs(cur, v):
+        def dfs(cur):
             """
+            拓扑排序: O(n)
             cur: current course index
             v: states: 0 = unkonwn, 1 == visiting, 2 = visited
+            return: True 有环, False 无环
             """
-            if v[cur] == self.State.visiting:
-                return True
-            if v[cur] == self.State.visited:
-                return False
-            
             v[cur] = self.State.visiting
             for t in graph_[cur]:
-                if dfs(t, v):
+                if v[t] == self.State.visiting:
                     return True
+                elif v[t] == self.State.unvisited:
+                    if dfs(t):
+                        return True
             v[cur] = self.State.visited
+            return False
+        
+        def circle(start, curr, v):
+            """
+            O(n2)
+            """
+            if start == curr and v[curr]:
+                return True
+            
+            for node in graph_[curr]:
+                if v[node]:
+                    continue
+                v[node] = True
+                if circle(start, node, v):
+                    return True
             return False
 
         graph_ = [[] for j in range(numCourses)]
@@ -34,11 +49,12 @@ class Solution:
         for pre in prerequisites:
             graph_[pre[0]].append(pre[1])
         
-        v = [0] * numCourses
+        v = [self.State.unvisited] * numCourses
 
         for i in range(numCourses):
-            if dfs(i, v):
-                return False
+            if v[i] != self.State.visited:
+                if dfs(i):
+                    return False
         return True
 # @lc code=end
 
