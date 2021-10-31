5d4f4ed530791b41a1ad252e78d7c1d6316be1fe
Jason He <hezhoux@qq.com>
2021-10-31 20:59:16 +0800

    Update 743.网络延迟时间.py

--- 743.网络延迟时间.py
+++ 743.网络延迟时间.py
@@ -8,13 +8,13 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         MAX_TIME = 101 * 100
-        dist = [MAX_TIME] * n
-        dist[k - 1] = 0 # start form k
-        for i in range(1, n):
+        dist = [MAX_TIME] * (n+1)
+        dist[k] = 0 # start form k
+        for i in range(1, n+1):
             for time in times:
-                u, v, w = time[0] - 1 ,time[1] - 1, time[2]
+                u, v, w = time[0] ,time[1], time[2]
                 dist[v] = min(dist[v], dist[u] + w)
-        max_dist = max(dist)
+        max_dist = max(dist[1:])
         return -1 if max_dist == MAX_TIME else max_dist
 
 # @lc code=end
