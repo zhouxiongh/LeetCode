e58364420389a30eff10cc7b2ad73a6dc632b369
Jason He <hezhoux@qq.com>
2021-12-06 14:58:03 +0800

    Update 684.冗余连接.py

--- Graph/684.冗余连接.py
+++ Graph/684.冗余连接.py
@@ -44,3 +44,38 @@ class UnionFindSet:
         return True
 # @lc code=end
 
+# second
+class UnionFindSet:
+    def __init__(self, n):
+        self._parents = [i for i in range(1, n+1)]
+        self._ranks = [1 for i in range(1, n+1)]
+
+    def find(self, u):
+        while u != self._parents[u]:
+            self._parents[u] = self._parents[self._parents[u]]
+            u = self._parents[u]
+        return u
+
+    def union(self, u, v):
+        # pu, pv = self._parents[u], self._parents[v]
+        pu, pv = self.find(u), self.find(v)
+        if pu == pv:
+            return False
+        if self._ranks[pu] > self._ranks[pv]:
+            self._parents[pv] = pu
+        elif self._ranks[pu] < self._ranks[pv]:
+            self._parents[pu] = pv
+        else:
+            self._parents[pu] = pv
+            self._ranks[pv] += 1
+        return True
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        uf = UnionFindSet(n)
+        for edge in edges:
+            if uf.union(edge[0], edge[1]) == False:
+                return edge
+        return None
+
