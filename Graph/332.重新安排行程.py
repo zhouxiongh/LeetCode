37ce165ad3f5615b2c26ab3db4449216c9d86a97
Jason He <hezhoux@qq.com>
2021-12-07 08:08:03 +0800

    Update 332.重新安排行程.py

--- Graph/332.重新安排行程.py
+++ Graph/332.重新安排行程.py
@@ -19,12 +19,39 @@ class Solution:
 
         for start, dest in tickets:
             trips[start].append(dest)
-        
+
         for trip in trips:
             heapq.heapify(trips[trip])
-        
+
         kStart = 'JFK'
         visit(kStart)
         return ans[::-1]
 # @lc code=end
+
+# version 2
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        def visit(start: str):
+            ends = routes[start]
+
+            while ends:
+                end = heapq.heappop(ends)
+                visit(end)
+            ans.append(start)
+
+        routes = collections.defaultdict(list)
+        ans = []
+        for start, end in tickets:
+            # routes[start].append[end]
+            routes[start].append(end)
+
+        for route in routes:
+            # heapq.heapify(route)
+            # heapq.heapify([route])
+            heapq.heapify(routes[route])
+
+        start_p = 'JFK'
+        visit(start_p)
+        return ans[::-1]
 
