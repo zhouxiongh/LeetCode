ba9b8bd471a9db5e5fd347bf985a2c078f8203d7
Jason He <hezhoux@qq.com>
2021-12-07 22:18:40 +0800

    Update 542.01-矩阵.py

--- Search/542.01-矩阵.py
+++ Search/542.01-矩阵.py
@@ -31,3 +31,29 @@ class Solution:
 
 # @lc code=end
 
+# version 2
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        row_size = len(mat)
+        col_size = len(mat[0])
+        queue = deque()
+        # seen = set()
+        ans = [[0] * col_size for i in range(row_size)]
+
+        for i in range(row_size):
+            for j in range(col_size):
+                if mat[i][j] == 0:
+                    # seen.add((i, j))
+                    queue.append((i, j))
+                else:
+                    mat[i][j] = -1
+
+        while queue:
+            first, second = queue.popleft()
+            for ni, nj in [(first-1, second), (first+1, second), (first, second-1), (first, second+1)]:
+                if ni < 0 or ni >= row_size or nj < 0 or nj >= col_size or mat[ni, nj] != -1:
+                    continue
+                # seen.add((ni, nj))
+                ans[ni][nj] = ans[first][second] + 1
+                queue.append((ni, nj))
+        return ans
