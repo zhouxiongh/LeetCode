448543c561af1efbb27e563e83c35578f3a2f3eb
Jason He <hezhoux@qq.com>
2021-12-07 11:04:35 +0800

    Update 40.组合总和-ii.py

--- Search/40.组合总和-ii.py
+++ Search/40.组合总和-ii.py
@@ -8,11 +8,11 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         def dfs(candidates, target, start, curr, ans):
-            # find 
-            if target == 0: 
+            # find
+            if target == 0:
                 ans.append(curr[:])
                 return
-            
+
             for i in range(start, len(candidates)):
                 # back up
                 if candidates[i] > target: return
@@ -21,11 +21,33 @@ class Solution:
                 curr.append(candidates[i])
                 dfs(candidates, target - candidates[i], i+1, curr, ans)
                 curr.pop()
-        
-        ans = []        
-        candidates.sort()        
+
+        ans = []
+        candidates.sort()
         dfs(candidates, target, 0, [], ans)
-        
+
         return ans
 # @lc code=end
 
+
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        def dfs(candidates, target, start, cur_ans, all_ans):
+            if target == 0:
+                all_ans.append(cur_ans[:])
+                return
+            for i in range(start, len(candidates)):
+                if candidates[i] > target:
+                    return
+                if i > start and candidates[i] == candidates[i-1]:
+                    continue
+                cur_ans.append(candidates[i])
+                dfs(candidates, target - candidates[i], i+1, cur_ans, all_ans)
+                cur_ans.pop()
+
+        all_ans = []
+        cur_ans = []
+        candidates.sort()
+        dfs(candidates, target, 0, cur_ans, all_ans)
+        return all_ans
