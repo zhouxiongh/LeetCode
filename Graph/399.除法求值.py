e862babc115e98999b9edab78388fb67e323500d
Jason He <hezhoux@qq.com>
2021-12-09 21:27:37 +0800

    Update 399.除法求值.py

--- Graph/399.除法求值.py
+++ Graph/399.除法求值.py
@@ -6,34 +6,64 @@
 from typing import List
 import collections
 # @lc code=start
+# class Solution:
+#     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+#         def divide(x, y, visited):
+#             """ dfs
+#             x / y = divide(x, n) * divide(n, y)
+#             """
+#             if x == y:
+#                 return 1.0
+#             visited.add(x)
+#             for n in graph_[x]:
+#                 if n in visited:
+#                     continue
+#                 visited.add(n)
+#                 ndy = divide(n, y, visited)
+#                 if ndy > 0:
+#                     return ndy * graph_[x][n]
+#             return -1.0
+#         ans = []
+#         graph_ = collections.defaultdict(dict)
+#         for (x, y), v in zip(equations, values):
+#             graph_[x][y] = v
+#             graph_[y][x] = 1.0 / v
+#         for x, y in queries:
+#             if x in graph_ and y in graph_:
+#                 ans.append(divide(x, y, set()))
+#             else:
+#                 ans.append(-1.0)
+#         return ans
 class Solution:
-    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+    def calcEquation(self, equations, values, queries):
         def divide(x, y, visited):
-            """ dfs
-            x / y = divide(x, n) * divide(n, y)
-            """
+            # x / y = x / n * n / y
             if x == y:
                 return 1.0
             visited.add(x)
-            for n in graph_[x]:
+            for n in graph[x]:
                 if n in visited:
                     continue
                 visited.add(n)
                 ndy = divide(n, y, visited)
                 if ndy > 0:
-                    return ndy * graph_[x][n]
+                    return ndy * graph[x][n]
             return -1.0
+
+        graph = defaultdict(dict)
         ans = []
-        graph_ = collections.defaultdict(dict)
-        for (x, y), v in zip(equations, values):
-            graph_[x][y] = v
-            graph_[y][x] = 1.0 / v
-        for x, y in queries:
-            if x in graph_ and y in graph_:
-                ans.append(divide(x, y, set()))
+        for (x, y), value in zip(equations, values):
+            graph[x][y] = value
+            graph[y][x] = 1.0 / value
+        for query in queries:
+            if query[0] in graph and query[1] in graph:
+                ans.append(divide(query[0], query[1], set()))
             else:
                 ans.append(-1.0)
+
+        
         return ans
+
 # @lc code=end
 equations = [["a","b"],["b","c"]]
 values = [2.0,3.0]
