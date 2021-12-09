a4c26cb911b2a86dcf64dc1ad8c5cc5af7a234db
Jason He <hezhoux@qq.com>
2021-12-09 16:59:26 +0800

    Update 378.有序矩阵中第-k-小的元素.py

--- Binary Search/378.有序矩阵中第-k-小的元素.py
+++ Binary Search/378.有序矩阵中第-k-小的元素.py
@@ -8,6 +8,7 @@
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         n = len(matrix)
+
         def check(mid):
             i, j = n - 1, 0
             num = 0
@@ -18,14 +19,45 @@ class Solution:
                 else:
                     i -= 1
             return num >= k
+
         l = matrix[0][0]
         r = matrix[-1][-1]
         while l < r:
-            m = l + (r-l) // 2
+            m = l + (r - l) // 2
             if check(m):
                 r = m
             else:
                 l = m + 1
         return l
-# @lc code=end
 
+
+# @lc code=end
+from typing import List
+
+
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        n = len(matrix)
+
+        def check(val: int):
+            i = n - 1
+            j = 0
+            num = 0
+            while i >= 0 and j <= n - 1:
+                if matrix[i][j] <= val:
+                    num += i
+                    j += 1
+                else:
+                    i -= 1
+            return num >= k
+
+        l = matrix[0][0]
+        r = matrix[-1][-1]
+        while l < r:
+            mid = l + (r - l) // 2
+            if check(mid):
+                r = mid
+            else:
+                l = mid + 1
+
+        return l
