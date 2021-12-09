c7848fdf13a9bd6404511ce43fe56fc3eefc306f
Jason He <hezhoux@qq.com>
2021-12-09 19:53:41 +0800

    Update 785.判断二分图.py

--- Graph/785.判断二分图.py
+++ Graph/785.判断二分图.py
@@ -5,28 +5,51 @@
 #
 
 # @lc code=start
+# class Solution:
+#     class Color:
+#         red = 1
+#         blue = -1
+#     def isBipartite(self, graph: List[List[int]]) -> bool:
+#         def coloring(node, color):
+#             """ dfs
+#             return: False if conflict
+#             """
+#             if colors[node]:
+#                 return colors[node] == color
+#             colors[node] = color
+#             for n in graph[node]:
+#                 if not coloring(n, -color):
+#                     return False
+#             return True
+#         n = len(graph)
+#         colors = [None] * n
+#         for i in range(n):
+#             if not colors[i]:
+#                 if not coloring(i, self.Color.red):
+#                     return False
+#         return True
 class Solution:
-    class Color:
-        red = 1
-        blue = -1
+    # class Color:
+    red = 1
+    green = -1
+
     def isBipartite(self, graph: List[List[int]]) -> bool:
         def coloring(node, color):
-            """ dfs
-            return: False if conflict
-            """
             if colors[node]:
                 return colors[node] == color
             colors[node] = color
-            for n in graph[node]:
-                if not coloring(n, -color):
+            for neighbor in graph[node]:
+                if coloring(neighbor, -color) == False:
                     return False
             return True
         n = len(graph)
         colors = [None] * n
         for i in range(n):
-            if not colors[i]:
-                if not coloring(i, self.Color.red):
+            if colors[i] is None:
+                if coloring(i, self.red) == False:
                     return False
         return True
+
+
 # @lc code=end
 
