e479a31f9a856d986d5b10a68a27cf6757a72eee
Jason He <hezhoux@qq.com>
2021-12-09 19:59:40 +0800

    Update 997.找到小镇的法官.py

--- Graph/997.找到小镇的法官.py
+++ Graph/997.找到小镇的法官.py
@@ -5,14 +5,25 @@
 #
 from typing import List
 # @lc code=start
+# class Solution:
+#     def findJudge(self, n: int, trust: List[List[int]]) -> int:
+#         degrees = [0] * (n+1)
+#         for t in trust:
+#             degrees[t[0]] -= 1
+#             degrees[t[1]] += 1
+#         for i in range(1, n+1):
+#             if degrees[i] == n - 1:
+#                 return i
+#         return -1
+
 class Solution:
-    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+    def findJudge(self, n, trust):
         degrees = [0] * (n+1)
-        for t in trust:
-            degrees[t[0]] -= 1
-            degrees[t[1]] += 1
+        for a, b in trust:
+            degrees[a] -= 1
+            degrees[b] += 1
         for i in range(1, n+1):
-            if degrees[i] == n - 1:
+            if degrees[i] == n-1:
                 return i
         return -1
 
