990860dfaf7fc77ecf394be856a34501156b77a5
hezhoux <hezhoux@qq.com>
2021-12-13 00:32:19 +0800

    Update 542.01-矩阵.py

--- Search/542.01-矩阵.py
+++ Search/542.01-矩阵.py
@@ -4,7 +4,6 @@
 # [542] 01 矩阵
 #
 
-# @lc code=start
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         row_size = len(mat)
@@ -29,7 +28,6 @@ class Solution:
                 queue.append((ni, nj))
         return ans
 
-# @lc code=end
 
 # version 2
 class Solution:
@@ -51,9 +49,36 @@ class Solution:
         while queue:
             first, second = queue.popleft()
             for ni, nj in [(first-1, second), (first+1, second), (first, second-1), (first, second+1)]:
-                if ni < 0 or ni >= row_size or nj < 0 or nj >= col_size or mat[ni, nj] != -1:
+                if ni < 0 or ni >= row_size or nj < 0 or nj >= col_size or mat[ni][nj] != -1:
                     continue
                 # seen.add((ni, nj))
                 ans[ni][nj] = ans[first][second] + 1
                 queue.append((ni, nj))
         return ans
+
+# version3
+# @lc code=start
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List:
+        row_size = len(mat)
+        col_size = len(mat[0])
+        ans = [[0] * col_size for i in range(row_size)]
+        queue = deque()
+
+
+        for i in range(row_size):
+            for j in range(col_size):
+                if mat[i][j] == 0:
+                    queue.append((i, j))
+        
+        while queue:
+            i, j = queue.popleft()
+            for ni, nj in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+                if ni < 0 or ni >= row_size or 0 > nj or nj >= col_size or mat[ni][nj] != 1:
+                    continue
+                mat[ni][nj] = -1
+                ans[ni][nj] = ans[i][j] + 1
+                queue.append((ni, nj))
+
+        return ans
+# @lc code=end
