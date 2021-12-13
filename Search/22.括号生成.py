99b4debbee54da16b9c152b365e8fad72fe01d9f
hezhoux <hezhoux@qq.com>
2021-12-13 16:52:20 +0800

    Update 22.括号生成.py

--- Search/22.括号生成.py
+++ Search/22.括号生成.py
@@ -4,7 +4,6 @@
 # [22] 括号生成
 #
 
-# @lc code=start
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         ans = []
@@ -27,7 +26,6 @@ class Solution:
 
         dfs(n, n, "")
         return ans
-# @lc code=end
 
 
 class Solution:
@@ -47,3 +45,22 @@ class Solution:
         ans = []
         dfs('', ans, 0, 0, n)
         return ans
+
+# @lc code=start
+class Solution:
+    def generateParenthesis(self, n):
+        def dfs(cur, ans, l=0, r=0):
+            if len(cur) == n * 2:
+                ans.append(cur)
+            if l < n:
+                cur += '('
+                dfs(cur, ans, l+1, r)
+                cur = cur[:-1]
+            if r < l:
+                cur += ')'
+                dfs(cur, ans, l, r+1)
+                cur = cur[:-1]
+        ans = list()
+        dfs('', ans)
+        return ans
+# @lc code=end
