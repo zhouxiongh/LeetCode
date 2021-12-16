2c54a15426e1591cd9e9026cb1cbff4d71ec22bc
hezhoux <hezhoux@qq.com>
2021-12-16 17:44:42 +0800

    Update 433. 最小基因变化.py

--- Search/433. 最小基因变化.py
+++ Search/433. 最小基因变化.py
@@ -1,3 +1,8 @@
+#
+# @lc app=leetcode.cn id=433 lang=python3
+#
+# [433] 最小基因变化
+#
 class Solution:
     def minMutation(self, start: str, end: str, bank: List[str]) -> int:
         def valid_mutation(s1, s2):
@@ -27,3 +32,33 @@ class Solution:
                         visited.add(b)
             ans += 1
         return ans
+        
+            
+# @lc code=start
+class Solution:
+    def minMutation(self, start, end, bank):
+        def valid_mutation(cur, mutate):
+            counter = 0
+            for c1, c2 in zip(cur, mutate):
+                if c1 != c2:
+                    counter += 1
+            return counter == 1
+        queue = deque()
+        queue.append(start)
+        visited = set()
+        ans = 0
+        while queue:
+            size = len(queue)
+            while size:
+                size -= 1
+                cur = queue.popleft()
+                if cur == end:
+                    return ans
+                for gene in bank:
+                    if valid_mutation(cur, gene) and gene not in visited:
+                        queue.append(gene)
+                        visited.add(gene)
+            ans += 1
+        return -1
+
+# @lc code=end
