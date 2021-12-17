48aac06de9e77d5314a83d73c27ce3a2e1bfb845
hezhoux <hezhoux@qq.com>
2021-12-17 11:02:53 +0800

    Update 684.冗余连接.py

--- Graph/684.冗余连接.py
+++ Graph/684.冗余连接.py
@@ -5,6 +5,8 @@
 #
 
 from typing import List
+
+
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         s = UnionFindSet(len(edges))
@@ -13,41 +15,44 @@ class Solution:
                 return edge
         return None
 
+
 class UnionFindSet:
     def __init__(self, n):
         # start from 0
         self._parents = [i for i in range(n + 1)]
         self._ranks = [1 for i in range(n + 1)]
-    
+
     def find(self, u):
         while u != self._parents[u]:
             self._parents[u] = self._parents[self._parents[u]]
             u = self._parents[u]
         return u
-    
+
     def union(self, u, v):
         """
         return: bool
         False if u, v already union
         """
         pu, pv = self.find(u), self.find(v)
-        if pu == pv: return False
-        
+        if pu == pv:
+            return False
+
         if self._ranks[pu] < self._ranks[pv]:
             self._parents[pu] = pv
         elif self._ranks[pu] > self._ranks[pv]:
             self._parents[pv] = pu
-        else:        
+        else:
             self._parents[pv] = pu
             self._ranks[pu] += 1
-        
+
         return True
+
 
 # second
 class UnionFindSet:
     def __init__(self, n):
-        self._parents = [i for i in range(1, n+1)]
-        self._ranks = [1 for i in range(1, n+1)]
+        self._parents = [i for i in range(1, n + 1)]
+        self._ranks = [1 for i in range(1, n + 1)]
 
     def find(self, u):
         while u != self._parents[u]:
@@ -69,6 +74,7 @@ class UnionFindSet:
             self._ranks[pv] += 1
         return True
 
+
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         n = len(edges)
@@ -78,7 +84,7 @@ class Solution:
                 return edge
         return None
 
-# @lc code=start
+
 class Solution:
     def findRedundantConnection(self, edges):
         n = len(edges)
@@ -88,10 +94,12 @@ class Solution:
             if uf.union(edge[0], edge[1]):
                 return edge
         return None
+
+
 class UnionFindSet:
     def __init__(self, n):
-        self._parents = [i for i in range(n+1)]
-        self._ranks = [1] * (n+1)
+        self._parents = [i for i in range(n + 1)]
+        self._ranks = [1] * (n + 1)
 
     def union(self, u, v):
         pu, pv = self.find(u), self.find(v)
@@ -110,5 +118,42 @@ class UnionFindSet:
             self._parents[u] = self._parents[self._parents[u]]
             u = self._parents[u]
         return u
+
+
+# @lc code=start
+class Solution:
+    def findRedundantConnection(self, edges):
+        n = len(edges)
+        uf = UnionFindSet(n)
+        for u, v in edges:
+            if not uf.union(u, v):
+                return (u, v)
+        return None
+
+
+class UnionFindSet:
+    def __init__(self, n):
+        self._parents = [i for i in range(n + 1)]
+        self._weights = [1] * (n + 1)
+
+    def union(self, u, v):
+        pu = self.find(u)
+        pv = self.find(v)
+        if pu == pv:
+            return False
+        if self._weights[pu] > self._weights[pv]:
+            self._parents[pv] = pu
+            self._weights[pv] += self._weights[pu]
+        else:
+            self._parents[pu] = pv
+            self._weights[pu] += self._weights[pv]
+        return True
+
+    def find(self, u):
+        while u != self._parents[u]:
+            self._parents[u] = self._parents[self._parents[u]]
+            u = self._parents[u]
+        return u
+
 
 # @lc code=end
