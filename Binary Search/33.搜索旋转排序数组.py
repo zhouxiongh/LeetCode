a3c3bec19130b25087b525b0acb384bf93c4c421
hezhoux <hezhoux@qq.com>
2021-12-19 21:02:37 +0800

    Update 33.搜索旋转排序数组.py

--- Binary Search/33.搜索旋转排序数组.py
+++ Binary Search/33.搜索旋转排序数组.py
@@ -4,7 +4,6 @@
 # [33] 搜索旋转排序数组
 #
 
-# @lc code=start
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l = 0
@@ -30,27 +29,48 @@ class Solution:
 
 
 
-# @lc code=end
 
 class Solution(object):
     """docstring for Solution"""
     def search(self, nums: List[int], target: int) -> int:
         lo = 0
         hi = len(nums)
-        while lo < hi:
+        while lo <= hi:
             mid = lo + (hi-lo) // 2
             if nums[mid] == target:
                 return mid
             # lo ... mid sorted
-            if nums[lo] < nums[mid]:
-                if nums[lo] <= target < nums[mid]:
-                    lo = mid + 1
-                else:
+            if nums[0] <= nums[mid]:
+                if nums[0] <= target < nums[mid]:
                     hi = mid - 1
+                else:
+                    lo = mid + 1
             # mid ... end sorted
             else:
-                if nums[mid] < target < nums[len(nums)-1]:
+                if nums[mid] < target <= nums[len(nums)-1]:
                     lo = mid + 1
                 else:
                     hi = mid - 1
         return -1
+
+# @lc code=start
+class Solution(object):
+    def search(self, nums: List[int], target: int) -> int:
+        lo = 0
+        hi = len(nums) - 1
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+            if target == nums[mid]:
+                return mid
+            if nums[0] <= nums[mid]:
+                if nums[0] <= target < nums[mid]:
+                    hi = mid - 1
+                else:
+                    lo = mid + 1
+            else:
+                if nums[mid] < target <= nums[len(nums)-1]:
+                    lo = mid + 1
+                else:
+                    hi = mid - 1
+        return -1
+# @lc code=end
