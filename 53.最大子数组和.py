dc46f8c87692f42149bdd377724eaa9a43e6a2a3
hezhoux <hezhoux@qq.com>
2021-12-20 15:06:55 +0800

    Update 53.最大子数组和.py

--- 53.最大子数组和.py
+++ 53.最大子数组和.py
@@ -4,7 +4,6 @@
 # [53] 最大子数组和
 #
 from typing import List
-# @lc code=start
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         ans = -float('inf')
@@ -16,6 +15,22 @@ class Solution:
             ans = max(dp[i], ans)
             i += 1
         return ans
-# @lc code=end
 nums = [-1]
 assert -1 == Solution().maxSubArray(nums)
+
+
+# @lc code=start
+class Solution:
+    def maxSubArray(self, nums):
+        n = len(nums)
+        dp = [0] * (n+1)
+        i = 1
+        ans = -float('inf')
+        for num in nums:
+            dp[i] = max(dp[i-1] + num, num)
+            ans = max(ans, dp[i])
+            i += 1
+        return ans
+
+
+# @lc code=end
