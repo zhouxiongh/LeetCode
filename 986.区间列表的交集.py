aa2c1c307a4b09e508abf286ffc9713a5b2d00b5
hezhoux <hezhoux@qq.com>
2021-12-21 17:09:42 +0800

    Update 986.区间列表的交集.py

--- 986.区间列表的交集.py
+++ 986.区间列表的交集.py
@@ -4,7 +4,6 @@
 # [986] 区间列表的交集
 #
 
-# @lc code=start
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
         ans = []
@@ -21,5 +20,20 @@ class Solution:
             else:
                 j += 1
         return ans
-# @lc code=end
 
+# @lc code=start
+class Solution:
+    def intervalIntersection(self, firstList, secondList):
+        ans = []
+        i = j = 0
+        while i < len(firstList) and j < len(secondList):
+            lo = max(firstList[i][0], secondList[j][0])
+            hi = min(firstList[i][1], secondList[j][1])
+            if lo <= hi:
+                ans.append([lo, hi])
+            if firstList[i][1] < secondList[j][1]:
+                i += 1
+            else:
+                j += 1
+        return ans
+# @lc code=end
