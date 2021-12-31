d7db48e6e33450488fc522b37b4a0b17754218b5
hezhoux <hezhoux@qq.com>
2021-12-31 11:17:23 +0800

    Update 40.组合总和-ii.py

--- Search/40.组合总和-ii.py
+++ Search/40.组合总和-ii.py
@@ -50,7 +50,6 @@ class Solution:
         dfs(candidates, target, 0, cur_ans, all_ans)
         return all_ans
 
-# @lc code=start
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         def dfs(candidates, target, start=0, cur_ans=[]):
@@ -72,4 +71,24 @@ class Solution:
 
 
 
+# @lc code=start
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        def dfs(start, target, ans, cur=[]):
+            if target == 0:
+                ans.append(cur[:])
+                return
+            for i in range(start, len(candidates)):
+                if candidates[i] > target:
+                    return
+                if i > start and candidates[i] == candidates[i-1]:
+                    continue
+                cur.append(candidates[i])
+                dfs(i+1, target-candidates[i], ans, cur)
+                cur.remove(candidates[i])
+            return
+        ans = []
+        candidates.sort()
+        dfs(0, target, ans)
+        return ans
 # @lc code=end
