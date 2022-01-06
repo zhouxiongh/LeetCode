b4637fca0d9c2f8e2a941351de5ff2d7520860fa
hezhoux <hezhoux@qq.com>
2022-01-06 11:53:53 +0800

    Create 71.简化路径.py

---
+++ 71.简化路径.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=71 lang=python3
+#
+# [71] 简化路径
+#
+
+# @lc code=start
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        return os.path.realpath(path)
+# @lc code=end
+
