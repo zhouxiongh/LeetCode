91fddac280cf15939b998e9ea5e8966c78c09401
hezhoux <hezhoux@qq.com>
2022-01-14 20:02:11 +0800

    Update 875.爱吃香蕉的珂珂.py

--- Binary Search/875.爱吃香蕉的珂珂.py
+++ Binary Search/875.爱吃香蕉的珂珂.py
@@ -7,37 +7,18 @@
 # @lc code=start
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        low_speed = 1
-        max_speed = max(piles)
-        target_hours = h
-        while low_speed < max_speed:
-            speed = low_speed + (max_speed - low_speed) // 2
-            eat_hours = 0
-            for p in piles:
-                eat_hours += (p + speed - 1) // speed
-            if eat_hours <= target_hours:
-                max_speed = speed
+        lo_speed = 1
+        hi_speed = max(piles)
+        while lo_speed < hi_speed:
+            m_speed = lo_speed + (hi_speed - lo_speed) // 2
+            time = 0
+            for pile in piles:
+                time += (pile + m_speed - 1) // m_speed
+            if time > h:
+                lo_speed = m_speed + 1
             else:
-                low_speed = speed + 1
-        return low_speed
+                hi_speed = m_speed
+        return lo_speed
+
 
 # @lc code=end
-
-
-class Solution:
-    def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        low_speed = 1
-        max_speed = max(piles)
-
-        while low_speed < max_speed:
-            guess_speed = low_speed + (max_speed - low_speed) // 2
-            eat_hours = 0
-            for pile in piles:
-                eat_hours += (pile + guess_speed - 1) // guess_speed
-            if eat_hours <= h:
-                max_speed = guess_speed
-            else:
-                low_speed = guess_speed + 1
-        return low_speed
-
-
