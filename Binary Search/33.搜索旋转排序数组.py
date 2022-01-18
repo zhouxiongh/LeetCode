542fad58d76253de66d2fa9d5c7639596937caf0
hezhoux <hezhoux@qq.com>
2022-01-18 17:13:47 +0800

    Update 33.搜索旋转排序数组.py

--- Binary Search/33.搜索旋转排序数组.py
+++ Binary Search/33.搜索旋转排序数组.py
@@ -60,17 +60,19 @@ class Solution(object):
         hi = len(nums) - 1
         while lo <= hi:
             mid = lo + (hi - lo) // 2
-            if target == nums[mid]:
+            if nums[mid] == target:
                 return mid
+            # 0 ... mid sorted
             if nums[0] <= nums[mid]:
                 if nums[0] <= target < nums[mid]:
                     hi = mid - 1
                 else:
                     lo = mid + 1
             else:
-                if nums[mid] < target <= nums[len(nums)-1]:
+                if nums[mid] < target <= nums[-1]:
                     lo = mid + 1
                 else:
                     hi = mid - 1
         return -1
+
 # @lc code=end
