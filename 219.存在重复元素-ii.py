7109f28da8a030eb15c106bb52c6c0c7d871e634
hezhoux <hezhoux@qq.com>
2022-01-19 14:37:59 +0800

    Create 219.存在重复元素-ii.py

---
+++ 219.存在重复元素-ii.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode.cn id=219 lang=python3
+#
+# [219] 存在重复元素 II
+#
+
+# @lc code=start
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        pos = {}
+        for i, num in enumerate(nums):
+            if num in pos:
+                if abs(pos[num] - i) <= k:
+                    return True
+            pos[num] = i
+        return False
+# @lc code=end
+
