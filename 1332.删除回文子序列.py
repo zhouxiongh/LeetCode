a2391678f88482a703f3d54e32209e603c51a42c
hezhoux <hezhoux@qq.com>
2022-01-22 13:45:37 +0800

    Create 1332.删除回文子序列.py

---
+++ 1332.删除回文子序列.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=1332 lang=python3
+#
+# [1332] 删除回文子序列
+#
+
+# @lc code=start
+class Solution:
+    def removePalindromeSub(self, s: str) -> int:
+        def is_palindrome(s):
+            return s == s[::-1]
+        return 1 if is_palindrome(s) else 2
+        
+# @lc code=end
+
